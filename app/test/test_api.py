c4f8b80746744e19c70a1cff21ccedecfddf54f9
Roberth <jossuebast@hotmail.com>
2026-06-29 23:38:36 -0500

    Feat(tests): test de integración para el control de parametros query en endpoint de consulta

--- app/test/test_api.py
+++ app/test/test_api.py
@@ -9,6 +9,9 @@ def test_read_root_api():
     assert respuesta.json() == {"status": "Running", "message": "Bienvenidos al Core del Sistema de Cajas de Ahorro"}
 
 def test_http_error_schema_pydantic_socios():
-    # Enviar un JSON vacío a la ruta de creación para forzar el fallo de validación de esquemas de Pydantic
     respuesta = client.post("/api/socios", json={})
-    assert respuesta.status_code == 422  # Unprocessable Entity (Error nativo de validación de FastAPI)
+    assert respuesta.status_code == 422  
+
+def test_get_consulta_movimientos_parametros_invalidos():
+    respuesta = client.get("/api/consulta-movimientos?cedula=12345&numero_cuenta=CTA-01")
+    assert respuesta.status_code == 422
